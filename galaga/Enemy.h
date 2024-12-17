3c334124b42455fa2da10afa0414a6ce84f633e9
dodoesdo <patrickwjyao@gmail.com>
2024-12-16 23:26:10 -0500

    enemies quick change + PNG uploaded

--- galaga/Enemy.h
+++ galaga/Enemy.h
@@ -1,5 +1,26 @@
 #pragma once
+#include <string>
+#include <vector>
+#include "Bullet.h"
+
+class Bullet;
+
 class Enemy
 {
+public:
+public:
+	Enemy(char* filename);
+	void draw();
+	void move(double direction, double deltaTime);
+	void shoot(std::vector<Bullet>& bullets);
+	void death(int frame);
+	int player_id;
+	int explosion_id[4];
+private:
+	double xPosition, yPosition;
+	double scale, angle;
+	double speed;
+};
+
 };
 
